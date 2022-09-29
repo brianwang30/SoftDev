da7bc24df3fb24fc22344f49992a45e30e8a246e
brianwang30 <bwang30@stuy.edu>
2022-09-29 13:56:42 -0400

    I love my puta, all my firends live in it

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -20,8 +20,9 @@ def choose(dictionary):
     #if class is not empty
     if len(dictionary[key]) > 0:
         #Return the randomly selected key along with a randomly selected element in the list associated with the key
-        return str(key) + ": " + random.choice(dictionary[key])
-    return "class " + str(key) + " is empty"
+        choice = random.choice(dictionary[key])
+        return "name: " + choice[0] + "\npd: " + key + "\nducky: " + choice[1]
+    return "empty"
 
 def addPd(pd, dictionary):
     dictionary[pd] = []
@@ -39,15 +40,30 @@ def rmStu(name, pd, dictionary):
     return "no student with " + name + " found"
 
 def populate(dic):
-    file = open("krewes.txt", "r")
+    f = open("krewes.txt", "r")
+    file = f.read()
+    dic_keys = [] #list of keys because we had some trouble with it
+               
+    #iterate through all the blocks of info
     while file.find("@@@") > -1:
-        devoInfo = file[0, file.find("@@@")]
+        #break up the blocks of info
+        devo_info = file[0 : file.find("@@@")]
         file = file[file.find("@@@") + 3 :]
-        devoPd = devoInfo[0, devoInfo.find("$$$")]
-        devoInfo = devoInfo[devoInfo.find("$$$") + 3 :]
-        devoName = devoInfo[0, devoInfo.find("$$$")]
-        devoInfo = devoInfo[devoInfo.find("$$$") + 3 :]
-        defoDucky = devoInfo
-        if dic.index(devoPd) 
+        devo_pd = devo_info[0 : devo_info.find("$$$")]
+        devo_info = devo_info[devo_info.find("$$$") + 3 :]
+        devo_name = devo_info[0 : devo_info.find("$$$")]
+        devo_info = devo_info[devo_info.find("$$$") + 3 :]
+        devo_ducky = devo_info
+        
+        #add the new entry
+        if devo_pd in dic_keys:
+            dic[devo_pd].append([devo_name, devo_ducky])
+        else:
+            dic[devo_pd] = [[devo_name, devo_ducky]]
+            dic_keys.append(devo_pd)
+            
 dictionary = {}
+populate(dictionary)
+#print(dictionary)
+print(choose(dictionary))
     
