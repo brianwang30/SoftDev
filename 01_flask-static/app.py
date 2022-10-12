d7b7e05c17288dc2352f08b96f06ce1bb63e9de9
unknown <bwang30@stuy.edu>
2022-10-12 19:29:00 -0400

    omg an actual bug fix message? I added heading and clarified some details.

---
+++ 01_flask-static/app.py
@@ -0,0 +1,19 @@
+# BWANG Brian Wang, April Li, Shreya Roy
+# SoftDev
+# Oct 2022
+
+# DEMO
+# basics of /static folder
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo quesoooooooooooooooooop!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
